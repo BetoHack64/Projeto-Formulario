f105daa6359fb5cd091565f2740b13d563925931
Jonathanintel <jonathandevintel@gmail.com>
2021-06-04 10:22:42 -0300

    form

---
+++ formulario/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Formulario</title>
+</head>
+<body>
+    <form action="config.php" method="post">
+    Nome: <input type="text" name="nome"></br>
+    Email: <input type="text" name="email"></br>
+    ID(Discord): <input type="text" name="iddiscord"></br>
+    Techs: <input type="text" name="techs">
+    <input type="submit">
+    </form>
+</body>
+</html>
